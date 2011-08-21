e222a95c8a006b6fc464ef815a96103ccd11b7d7
Bernard Potocki <bernard.potocki@imanel.org>
2011-08-21 20:23:31 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnifiedDB::Application.config.secret_token = 'f6f6cd78006bf1e24fa5d5475e94cb579699d31c03743758d80e24c9487f3c603b0670bc9184054101c9af6d3ec7caa26dad0b04c0867c80f706169e93c4cb06'
