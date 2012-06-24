bd1dbdd749dd5f6a2adf85f93f99df0fd0911218
Bernard Potocki <bernard.potocki@imanel.org>
2012-06-24 21:45:25 +0200

    Use anonymouse for querying imdb

--- lib/extensions/imdb.rb
+++ lib/extensions/imdb.rb
@@ -3,10 +3,15 @@ require 'imdb'
 module Imdb
   class Search < MovieList
 
+    def self.query(query)
+      open("http://anonymouse.org/cgi-bin/anon-www.cgi/http://akas.imdb.com/find?q=#{CGI::escape(query)};s=tt")
+    end
+
     def parse_movie
       id                 = document.at("head/link[@rel='canonical']")['href'][/\d+/]
       title              = document.at("h1").innerHTML.imdb_strip_tags.gsub("\n", " ").strip.imdb_unescape_html
       [Imdb::Movie.new(id, title)]
     end
+
   end
 end
